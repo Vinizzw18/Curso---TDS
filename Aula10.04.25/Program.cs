d462c86f49fd968093f0aab149cce6e34ba693fe
Vgz <vinigames1122@gmail.com>
2025-04-10 15:22:57 -0300

    Add files via upload

---
+++ Aula10.04.25/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+
+string[] nomes = { "Pedro", "Cachorro", "Gato", "Lino", "Gabriel" };
+
+string novonome = "";
+WriteLine("Insira um nome a mais: ");
+novonome = Convert.ToString(ReadLine());
+List<string> novosnomes = new List<string>(nomes.ToList());
+novosnomes.Add(novonome);
+nomes = novosnomes.ToArray();
+Array.Sort(nomes);
+
+foreach (var item in nomes)
+{
+    WriteLine($"\nNome adicionado: {item}");
+}
+ReadLine();
+
